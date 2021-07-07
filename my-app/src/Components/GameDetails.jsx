18cbad1e54cb94bcd9a783952be835e385120774
jwhong <jwhong98@gmail.com>
2021-07-06 17:40:55 -0700

    Added styling and set data paths

--- my-app/src/Components/GameDetails.jsx
+++ my-app/src/Components/GameDetails.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const GameDetails = (props) => {
     return(
-        <div>
+        <div id="details">
             <p>{props.device}</p>
             <h2>{props.title}</h2>
             <p>{props.availability}</p>
             <button>Add to Wish List</button>
 
-            <div>
+            <div id="purchase">
                 <h2>{props.price}</h2>
                 <button className="btn btn-danger">Buy digital</button>
                 <p>Eligible for up to {props.points} points</p>
--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -4,12 +4,14 @@ import Navbar from "./Navbar";
 const Header = () => {
     return (
         <header>
-        <img alt="Nintendo Logo" src="https://1000logos.net/wp-content/uploads/2021/04/Nintendo-logo.png" id="logo"></img>
-        <button id="search">Search games, systems, support, etc.</button>
-        <div id="headerButton">
-            <button className="navButton">Support</button>
-            <button className="navButton">Wish List</button>
-            <button className="navButton">Log in / Sign up</button>
+        <div className="head">
+            {/* <img alt="Nintendo Logo" src="https://1000logos.net/wp-content/uploads/2021/04/Nintendo-logo.png" id="logo"></img> */}
+            <button id="search">Search games, systems, support, etc.</button>
+            <div id="headerButton">
+                <button className="navButton">Support</button>
+                <button className="navButton">Wish List</button>
+                <button className="navButton">Log in / Sign up</button>
+            </div>
         </div>
         
             <Navbar/>
